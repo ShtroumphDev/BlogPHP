4b417559c72078a0e8460feb71617ff9d0fda485
DESKTOP-9MTB132\Aurel <demblans.dev@gmail.com>
2024-05-30 17:39:02 +0200

    show pending comments

--- src/Controller/BackOffice/HomePageController.php
+++ src/Controller/BackOffice/HomePageController.php
@@ -2,10 +2,25 @@
 
 namespace App\Controller\BackOffice;
 
+use App\Repository\CommentsRepository;
+use App\Repository\UserRepository;
+
 class HomePageController extends BackOfficeController
 {
 	public function index()
 	{
+		$commentRepository = new CommentsRepository();
+		$userRepository    = new UserRepository();
+		$pendingComments   = $commentRepository->findBy(['state'=> 'pending']);
+
+		$commentUserNames =[];
+
+		foreach ($pendingComments as $comment) {
+			$userId                    = $comment->getUserId();
+			$userName                  = $userRepository->find($userId)->getPseudo();
+			$commentUserNames[$userId] = $userName;
+		}
+
 		ob_start();
 		require_once 'src/Templates/HomePageBack.html';
 		$content = ob_get_clean();
