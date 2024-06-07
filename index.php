9bd9e508918a718c1681bd99fae1a48de036afe4
DESKTOP-9MTB132\Aurel <demblans.dev@gmail.com>
2024-06-07 13:59:58 +0200

    remove useless route

--- index.php
+++ index.php
@@ -19,15 +19,7 @@ $router->get('/', 'FrontOffice#HomePageController#index');
 $router->get('/article/:id', 'FrontOffice#PostController#showOnePost');
 $router->get('/articles/tous-les-articles', 'FrontOffice#PostController#showAllPosts');
 $router->get('/articles/categorie-:id', 'FrontOffice#PostController#showAllPostsByCategory');
-$router->get('/posts/:id-:slug', function ($id, $slug) use ($router) {
-	echo $router->url('post.show', ['id' => 1, 'slug' => 'salut-les-gens']);
-}, 'post.show')
-->with('id', '[0-9]+')
-->with('slug', '[a-z\-0-9]+');
 $router->get('/posts/:id', 'FrontOffice#HomePageController#index');
-$router->post('/posts/:id', function ($id) {
-	echo 'je poste larticle numero ID' . $id;
-});
 $router->post('/add-user', 'FrontOffice#UserController#add');
 $router->post('/connexion', 'FrontOffice#AuthenticationController#logIn');
 $router->get('/deconnexion', 'FrontOffice#AuthenticationController#logOut');
