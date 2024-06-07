ae290e1562bb1563ae8df5f277de35e0036e11c7
ShtroumphDev <shtroumph_nolife@hotmail.fr>
2024-06-07 12:21:00 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use App\Constants;
+use App\Router\Router;
+use Dotenv\Dotenv;
+
+session_start();
+if (empty($_SESSION['token'])) {
+	$_SESSION['token'] = bin2hex(random_bytes(32));
+}
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$router = new Router($_GET['url']);
+$router->get('/', 'FrontOffice#HomePageController#index');
+$router->get('/article/:id', 'FrontOffice#PostController#showOnePost');
+$router->get('/articles/tous-les-articles', 'FrontOffice#PostController#showAllPosts');
+$router->get('/articles/categorie-:id', 'FrontOffice#PostController#showAllPostsByCategory');
+$router->get('/posts/:id-:slug', function ($id, $slug) use ($router) {
+	echo $router->url('post.show', ['id' => 1, 'slug' => 'salut-les-gens']);
+}, 'post.show')
+->with('id', '[0-9]+')
+->with('slug', '[a-z\-0-9]+');
+$router->get('/posts/:id', 'FrontOffice#HomePageController#index');
+$router->post('/posts/:id', function ($id) {
+	echo 'je poste larticle numero ID' . $id;
+});
+$router->post('/add-user', 'FrontOffice#UserController#add');
+$router->post('/connexion', 'FrontOffice#AuthenticationController#logIn');
+$router->get('/deconnexion', 'FrontOffice#AuthenticationController#logOut');
+$router->post('/ajouter-commentaire', 'FrontOffice#CommentController#addComment', null, true, Constants::SUBSCRIBER);
+$router->get('/retirer-commentaire/:id', 'FrontOffice#CommentController#removeComment', null, true, Constants::SUBSCRIBER);
+$router->get('/administration', 'BackOffice#HomePageController#index', null, true, Constants::MODERATOR);
+$router->get('/administration/valider-commentaire/:id', 'BackOffice#CommentController#validateComment', null, true, Constants::MODERATOR);
+$router->get('/administration/rejeter-commentaire/:id', 'BackOffice#CommentController#rejectComment', null, true, Constants::MODERATOR);
+$router->get('/administration/creer-article', 'BackOffice#PostController#createPost', null, true, Constants::ADMIN);
+$router->post('/administration/ajouter-article', 'BackOffice#PostController#addPost', null, true, Constants::ADMIN);
+$router->get('/administration/liste-article', 'BackOffice#PostController#showListPost', null, true, Constants::ADMIN);
+$router->get('/administration/modifier-article/:id', 'BackOffice#PostController#updatePost', null, true, Constants::ADMIN);
+$router->post('/administration/modifier-article', 'BackOffice#PostController#patchPost', null, true, Constants::ADMIN);
+
+try {
+	$router->run();
+} catch (\Throwable $error) {
+	include_once './src/templates/Error404.html';
+}
---
+++ readme.txt
@@ -0,0 +1,26 @@
+README pour le premier blog en php
+
+installation du projet : 
+
+- il faut avoir un moyen de lancer un serveur local de développement (wamp64 par exemple)
+- dans le répertoire "www" pour wamp64 ou bien dans le répertoire correspondant de votre serveur 
+    copier le projet avec git => "git clone https://github.com/AurelienCorp/blogPHP.git"
+- créer une base de données avec le fichier sql fournis dans le dossier SQL_Database
+- mettre à jours les données présentent dans le .env avec les données que vous avez en local pour la base de données
+- avec le serveur lancer le localhost correspondant au projet
+
+Fonctionnement du site :
+
+- via la page d'accueil vous pouvez consulter les 3 articles les plus récents publiés sur le site
+- vous pouvez télécharger le CV au format PDF
+- accès au réseaux sociaux
+- via la barre de navigation vous avez accès à la liste de tout les articles ou bien des articles par catégories
+- vous pouvez vous inscrire sur le site en tant qu'abonné en remplissant le formulaire qui se trouve en bas de page
+    cela vous donne le droit des laisser des commentaires sur les articles
+- la connection à votre compte se trouve dans la barre de navigation en haut a droite 
+
+Administration : 
+
+- si vous avez les droits administrateur vous pouvez accéder à la partie administration en cliquant sur le lien dans le pied de page
+- sur la page d'accueil de l'administation vous avez accès à la gestion des commentaires ainsi qu'à la création et modification des articles
+- vous pouvez revenir sur le site en cliquant sur le lien en bas de page
